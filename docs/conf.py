c0492bb7ab1bdca99ab3bbacbb1126cb82f92bd8
Ask Haugerud Hovik <ask.hovik@gmail.com>
2026-06-14 15:41:13 +0200

    another try

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ copyright = f"{date.today().year}, {author}"
 
 extensions = [
     "myst_parser",
-    "autoapi.sphinx",
+    "autoapi.extension",
     "numpydoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.graphviz",
